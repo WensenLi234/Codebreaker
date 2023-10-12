3d1609449bfad92f94aca81706e357043e0fb7c7
WensenLi234 <wli9204@bths.edu>
2023-10-12 00:26:39 -0400

    Implemented clear button

--- script.js
+++ script.js
@@ -3,38 +3,61 @@ const buttons = [
     document.getElementById("buttonTwo"),
     document.getElementById("buttonThree")
 ];
-let digitValues = [
-    1,
-    1,
-    1
-];
+const clearButton = document.getElementById("buttonClear")
+let correctCombination = [];
+let digitValues = [];
 const codeDigits = [
     document.getElementById("digitOne"),
     document.getElementById("digitTwo"),
     document.getElementById("digitThree")
 ];
 let turnsLeft = 7;
+let digitPlace = 0;
 let minutesLeftDisplay = document.getElementById("minutesLeftDisplay")
-function init() {
-    turnsLeft = 7;
+function clear() {
+    digitPlace = 0;
     digitValues = [
         1,
         1,
         1,
-    ];
+    ],
+    updateDisplay();
+}
+function init() {
+    turnsLeft = 7;
+    clear();
+    for(let i = 0; i < buttons.length; i++) {
+        console.log(buttons);
+        buttons[i].addEventListener("click", function(){setDigit(i + 1)});
+    }
+    clearButton.addEventListener("click", clear())
     updateDisplay();
 }
 function updateDisplay() {
     for(let i = 0; i < digitValues.length; i++) {
         codeDigits[i].textContent = digitValues[i];
     }
-    
+    minutesLeftDisplay.textContent = turnsLeft;
 }
-function addToDigit(digit) {
-    if(digitValues[digit] == 3) {
-        
+function shiftDigit() {
+    if(digitPlace != codeDigits.length - 1) {
+        digitPlace += 1;
+    } else {
+        digitPlace = 0;
+        processGuess(digitValues);
     }
 }
-for(let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", )
+function processGuess(digits) {
+    
 }
+function decrementMinutesLeft() {
+    turnsLeft -= 1;
+    updateDisplay();
+}
+function setDigit(value) {
+    digitValues[digitPlace] = value
+    shiftDigit();
+    updateDisplay();
+}
+console.log(clearButton);
+addEventListener("load", init);
