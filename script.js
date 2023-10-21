d11a9ea2b2ea4b4cebdba23d937dabc7d0816a4c
WensenLi234 <wli9204@bths.edu>
2023-10-20 21:45:50 -0400

    added event log, and clock

--- script.js
+++ script.js
@@ -4,60 +4,85 @@ const buttons = [
     document.getElementById("buttonThree")
 ];
 const clearButton = document.getElementById("buttonClear")
+const combinationDisplay = document.getElementById("vaultCode")
+const log = document.getElementById("log")
 let correctCombination = [];
 let digitValues = [];
-const codeDigits = [
-    document.getElementById("digitOne"),
-    document.getElementById("digitTwo"),
-    document.getElementById("digitThree")
-];
 let turnsLeft = 7;
 let digitPlace = 0;
-let minutesLeftDisplay = document.getElementById("minutesLeftDisplay")
+const minutesLeftDisplay = document.getElementById("minutesLeftDisplay")
 function clear() {
     digitPlace = 0;
     digitValues = [
-        1,
-        1,
-        1,
+
     ],
     updateDisplay();
 }
 function init() {
-    turnsLeft = 7;
-    clear();
     for(let i = 0; i < buttons.length; i++) {
         console.log(buttons);
         buttons[i].addEventListener("click", function(){setDigit(i + 1)});
     }
     clearButton.addEventListener("click", clear);
+    newGame();
+}
+function newGame() {
+    turnsLeft = 7;
+    clear();
+    randomizeCombination();
+    console.log(correctCombination)
     updateDisplay();
 }
+function randomizeCombination() {
+    correctCombination = [
+        Math.floor(Math.random() * 3 + 1),
+        Math.floor(Math.random() * 3 + 1),
+        Math.floor(Math.random() * 3 + 1)
+    ]
+}
 function updateDisplay() {
+    let combination = ""
     for(let i = 0; i < digitValues.length; i++) {
-        codeDigits[i].textContent = digitValues[i];
+        if(digitValues[i] != 0) {
+            combination += digitValues[i]
+        }
     }
+    combinationDisplay.textContent = combination;
     minutesLeftDisplay.textContent = turnsLeft;
 }
 function shiftDigit() {
-    if(digitPlace != codeDigits.length - 1) {
+    if(digitPlace != 2) {
         digitPlace += 1;
     } else {
         digitPlace = 0;
-        processGuess(digitValues);
+        if(processGuess(digitValues)) {
+            console.log("game won")
+        } else {
+            decrementMinutesLeft()
+        }
+        clear()
     }
 }
 function processGuess(digits) {
-
+    for(var i = 0; i < digits.length; i++) {
+        if(digits[i] != correctCombination[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 function decrementMinutesLeft() {
     turnsLeft -= 1;
     updateDisplay();
+    logMessage("Minutes minus")
 }
 function setDigit(value) {
     digitValues[digitPlace] = value
     shiftDigit();
     updateDisplay();
 }
-console.log(clearButton);
+function logMessage(message) {
+    newLog = log.innerHTML + message + '<br/>';
+    log.innerHTML = newLog
+}
 addEventListener("load", init);
